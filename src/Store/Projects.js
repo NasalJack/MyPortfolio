4cb5ec14f1e6567c0355bfa26c8bcc6da1c7c369
Sam Walker <sam.walker@comcast.net>
2019-11-14 14:07:42 -0500

    added CSS and HTML to builtWith

--- src/Store/Projects.js
+++ src/Store/Projects.js
@@ -3,7 +3,7 @@ const Projects = [
     name: 'Gameplay Valet',
     title: 'Gameplay Valet',
     screenshot: 'GameplayValet',
-    builtWith: 'Javascript, React, PostgreSQL, Knex, Node, Express',
+    builtWith: 'HTML, CSS, Javascript, React, PostgreSQL, Knex, Node, and Express',
     content: "Gameplay Valet is intended to be a personal resource for frequent boardgamers. It serves as a repository of rules, tips, and notes for games you own (which you can track with a personalized list) or any game that exists in its database.",
     page: 'https://gameplayvalet.now.sh/',
     repo: 'https://github.com/NasalJack/Gameplay-Valet-Client'
@@ -12,7 +12,7 @@ const Projects = [
     name: 'GoodMeal',
     title: 'GoodMeal',
     screenshot: 'GoodMeal',
-    builtWith: 'Javascript, React, PostgreSQL, Knex, Firebase, Node, Express',
+    builtWith: 'HTML, CSS, Javascript, React, PostgreSQL, Knex, Firebase, Node, and Express',
     content: "GoodMeal is an app for creating and storing recipes along with a host of periphery functionalities, like creating shopping lists from a recipe based on what you need and already own or checking your owned ingredients against recipes to find what you can make with what you have on hand.",
     page: 'https://good-meal.now.sh/',
     repo: 'https://github.com/thinkful-ei-firefly/recipe-client'
@@ -21,7 +21,7 @@ const Projects = [
     name: 'Spaced Repetition',
     title: 'Spaced Repetition',
     screenshot: 'SpacedRepetition',
-    builtWith: 'Javascript, React, PostgreSQL, Knex, Node, Express',
+    builtWith: 'HTML, CSS, Javascript, React, PostgreSQL, Knex, Node, and Express',
     content: "Spaced Repetition is an app for learning words from a language. It uses an algorithm based on the number of times you get a word right/wrong to determine how often to quiz you on that word in order to reinforce words you struggle with while tapering off on displaying words you know quite well.",
     page: 'https://spaced-repetition-sam-ahmed.now.sh/',
     repo: 'https://github.com/thinkful-ei-firefly/spaced-repetition-sam-ahmed-client'
@@ -30,7 +30,7 @@ const Projects = [
     name: 'Petful',
     title: 'Petful',
     screenshot: 'Petful',
-    builtWith: 'Javascript, React, Node',
+    builtWith: 'HTML, CSS, Javascript, React, and Node',
     content: "Petful is an app for adopting from a hypothetical animal shelter where only the cat and dog that have been with the shelter the longest are available for adoption at any given time. As part of a challenge, this app was built in a single day.",
     page: 'https://petful-sam-richard.now.sh/pets',
     repo: 'https://github.com/thinkful-ei-firefly/petful-client-sam-richard'
@@ -39,7 +39,7 @@ const Projects = [
     name: 'Magic Quiz',
     title: 'Magic Quiz App',
     screenshot: 'magicQuiz',
-    builtWith: 'Javascript and jQuery',
+    builtWith: 'HTML, CSS, Javascript and jQuery',
     content: 'This is a basic quiz app that loads a quiz at random from a repository of quizzes. The questions all pertain to the Magic: The Gathering card game. As a bonus I also designed a page to make writing additional quizzes easier with a form that you can fill out and have it output the result into the console in a form that can be easily copy/pasted into the repository of quizzes.',
     page: 'https://nasaljack.github.io/magic-quiz-app/',
     repo: 'https://github.com/NasalJack/magic-quiz-app'
@@ -48,7 +48,7 @@ const Projects = [
     name: 'Buffy Quiz',
     title: 'Buffy Quiz App',
     screenshot: 'buffyQuiz',
-    builtWith: 'Javascript and jQuery',
+    builtWith: 'HTML, CSS, Javascript and jQuery',
     content: 'This is a quiz that tests your knowledge of Buffy: the Vampire Slayer trivia. Each time the quiz loads five questions are chosen at random from the repository of possible questions, and the order of the possible answers to each question are randomized as well. No two quizes will ever be the exact same.',
     page: 'https://thinkful-ei-firefly.github.io/Quiz-App-Sam-Ben/',
     repo: 'https://github.com/thinkful-ei-firefly/Quiz-App-Sam-Ben'
@@ -57,7 +57,7 @@ const Projects = [
     name: 'Bookmarker',
     title: 'Bookmark App',
     screenshot: 'bookmark',
-    builtWith: 'Javascript, jQuery, and AJAX',
+    builtWith: 'HTML, CSS, Javascript, jQuery, and AJAX',
     content: 'This is an app that allows users to save bookmarks for websites along with a rating of the site and a brief description of it. The user can add or delete bookmarks, as well as filter their current view based on the rating of webtsites they would like to see.',
     page: 'https://thinkful-ei-firefly.github.io/bookmark-app-sam/',
     repo: 'https://github.com/thinkful-ei-firefly/bookmark-app-sam'
