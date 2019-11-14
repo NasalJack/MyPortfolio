1acb62b30f81707b18661c4dbaf138c0479de992
Sam Walker <sam.walker@comcast.net>
2019-11-14 14:29:25 -0500

    added project link on landing and lowered scale increase on project hover

--- src/Routes/Landing/Landing.js
+++ src/Routes/Landing/Landing.js
@@ -9,6 +9,7 @@ const Landing = () => {
       <div className="intro">Hi, my name is Sam and I'm a full stack developer</div>
       <img className="hero" src={hero} alt="Me standing in front of a lake" />
       <Link to='/about'><button className="about-me-link">Learn More</button></Link>
+      <Link to='/projects'><button>View Projects</button></Link>
     </div>
   )
 }
