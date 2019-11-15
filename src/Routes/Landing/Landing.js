581955cc860fc4331c0a00a2b1324d61ea750928
Sam Walker <sam.walker@comcast.net>
2019-11-15 14:30:48 -0500

    updated hero

--- src/Routes/Landing/Landing.js
+++ src/Routes/Landing/Landing.js
@@ -6,8 +6,9 @@ import hero from './hero.png'
 const Landing = () => {
   return (
     <div className='Landing'>
-      <div className="intro">Hi, my name is Sam and I'm a full stack developer</div>
+      <div className="intro">Sam Walker</div>
       <img className="hero" src={hero} alt="Me standing in front of a lake" />
+      <div className="job">Full Stack Developer</div>
       <Link to='/about'><button className="button about-me-link">Learn More</button></Link>
       <Link to='/projects'><button className="button">View My Projects</button></Link>
     </div>
